7899eada4bf167884ed5143737f08a8462afe4f2
vladh <vlad.hagai@mai.ru>
2024-02-05 22:55:33 +0300

    TIK_3: models, views

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -11,6 +12,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
     def __str__(self):
         return self.name
 
@@ -22,7 +26,7 @@ class Product(models.Model):
         on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='',  # Указать адресс папки с продуктами.
+    image = models.ImageField(upload_to='products/%Y/%m/%d',  # Указать адресс папки в которую будем отгружать.
                               blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10,
@@ -38,6 +42,9 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', kwargs={'id': self.id, 'slug': self.slug})
 
     def __str__(self):
         return self.name
