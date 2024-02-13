442d4dc6a5402d871b14e98a5441a082fb8fe0de
vladh <vlad.hagai@mai.ru>
2024-02-13 23:12:49 +0300

    TIK_10: Orders

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -16,10 +16,7 @@ def order_create(request):
                                         product=item['product'],
                                         price=item['price'],
                                         quantity=item['quantity'])
-            # clear the cart
             cart.clear()
-            # launch asynchronous task
-            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
