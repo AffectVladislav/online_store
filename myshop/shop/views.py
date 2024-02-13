c362b65008ac740f3a1dd964150be6cd73da3e93
vladh <vlad.hagai@mai.ru>
2024-02-13 20:57:23 +0300

    TIK_9: Orders

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -15,6 +15,6 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    cart_product_form = CartAddProductForm()
+    cart_product_form = CartAddProductForm()    # кнопка добавить в корзину
     return render(request, 'shop/product/detail.html', {'product': product,
                                                         'cart_product_form': cart_product_form})
